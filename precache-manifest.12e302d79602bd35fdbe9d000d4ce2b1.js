e61407f952a65b6bbf61b63341227b72e364ff8a
Basil Koval <lodupu8989@g,gmail.com>
2021-02-01 10:50:46 +0200

    Updates

---
+++ precache-manifest.12e302d79602bd35fdbe9d000d4ce2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f35505520d31950896e276ced63ce7",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "5d323a3e3ac1aceb2e05",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.bbd6c3ad.chunk.css"
+  },
+  {
+    "revision": "7f9c14586cd53b7dc7f6",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.6ce9eb8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.6ce9eb8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d323a3e3ac1aceb2e05",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.6e1aff05.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
